729c569a4d0c706441d7c4db3158f201e72c2f89
Elias Engström <elias.engstrom@elev.ntig.se>
2025-03-03 14:42:22 +0100

    gjort en epostapp

---
+++ Epostappen/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Net.Mail;
+using System.Net;
+
+
+namespace Epostappen;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSkicka(object sender, RoutedEventArgs e)
+    {
+        // Läs av epost & meddelanden
+        string epost = tbxEpost.Text;
+        string meddelande = tbxMeddelande.Text;
+
+        // Sanity check
+        if (epost != "" && meddelande != "")
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("", "");
+            smtp.Send();
+
+        }
+        else
+        {
+           lblStatus.Content = "UHUHUH"; 
+        }
+    }
+}
